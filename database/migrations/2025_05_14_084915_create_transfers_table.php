8917b962e65f6bbae0bf5b8cd955bcd2549b7e17
amineElgaini <amineelgainisocial@gmail.com>
2025-05-14 10:06:57 +0100

    create migrations user, branches, customer, employee, transfer, transaction

---
+++ database/migrations/2025_05_14_084915_create_transfers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transfers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('from_customer_id')->constrained('customers')->onDelete('cascade');
+            $table->foreignId('to_customer_id')->constrained('customers')->onDelete('cascade');
+            $table->decimal('amount', 15, 2);
+            $table->foreignId('performed_by')->nullable()->constrained('employees')->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transfers');
+    }
+};
